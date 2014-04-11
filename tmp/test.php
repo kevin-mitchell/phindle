9b64ef2f6bfedb9ba3f3ff84bcce4e16b2cc916e
develpr <kevin@develpr.com>
2014-04-11 00:12:41 -0700

    add some working test scripts, working on full path to get skeleton in place.

---
+++ tmp/test.php
@@ -0,0 +1,44 @@
+<?php
+require_once('../vendor/autoload.php');
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use \Develpr\Phindle\Phindle;
+use \Develpr\Phindle\Content;
+use \Develpr\Phindle\FileHandler;
+
+$html1 = file_get_contents(__DIR__ . '/UG-C3.html');
+$html2 = file_get_contents(__DIR__ . '/UG-C6.html');
+
+//$fileHandler = new FileHandler(__DIR__, 'blahblah');
+
+$phindle = new Phindle(array(
+	'title' => "First Mobi Book",
+	'publisher' => "Develpr",
+	'creator' => 'Kevin Mitchell',
+	'language' => \Develpr\Phindle\OpfRenderer::LANGUAGE_ENGLISH_US,
+	'subject' => 'Computers',
+	'description' => 'A great book about stuff',
+	'path'	=> __dir__
+));
+
+
+
+$content = new Content();
+
+$content->setHtml($html1)->setTitle('Title 1')->setPosition(1)->setUniqueIdentifier('page_1');
+
+$content1 = new Content();
+
+$content1->setHtml($html2)->setTitle('Title 2')->setPosition(2)->setUniqueIdentifier('page_2');
+
+$phindle->addContent($content1);
+$phindle->addContent($content);
+
+
+
+
+$phindle->process();
+
+$hi = "HI";
