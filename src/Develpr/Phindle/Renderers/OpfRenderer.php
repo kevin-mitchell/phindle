f6e21c7e0a89a9acc1cdf180713eeb1af3fa6e28
develpr <kevin@develpr.com>
2014-04-13 01:30:40 -0700

    Fleshing out opf renderer

--- src/Develpr/Phindle/Renderers/OpfRenderer.php
+++ src/Develpr/Phindle/Renderers/OpfRenderer.php
@@ -18,6 +18,144 @@ class OpfRenderer extends XmlRenderer{
 	const LANGUAGE_SPANISH = 'es';
 	const LANGUAGE_FRENCH = 'fr';
 
+	const MANIFEST_ITEM_PREFIX = 'item';
+
+	private $htmlParser;
+	private $content;
+
+	/**
+	 * Construction for OpfRenderer requires a html element parser for building list of resources
+	 *
+	 * @param Templatish $templatish
+	 * @param HtmlElementExtractor $htmlParser
+	 */
+	public function __construct(Templatish $templatish = null, HtmlElementExtractor $htmlParser = null)
+	{
+		parent::__construct($templatish);
+
+		if(is_null($htmlParser))
+			$this->htmlParser = new HtmlElementExtractor();
+		else
+			$this->htmlParser = $htmlParser;
+	}
+
+	public function render(array $attributes, array $content)
+	{
+		$this->data = $attributes;
+		$this->content = $content;
+
+		$this->prepareForRender();
+
+		$this->templatish->setData($this->data);
+		$this->templatish->setTemplate('open_packaging_format.opf');
+		$opfContent = $this->templatish->buildTemplate();
+
+		return $opfContent;
+	}
+
+	/**
+	 * Prepare the opf related data/fields for rendering
+	 */
+	protected function prepareForRender()
+	{
+		if(!$this->getValue('publicationDate'))
+			$this->setValue('publicationDate', date("Y-m-d"));
+
+		//If a static resource path is set (for css/images) then we want to make sure we provided a uniform path to prepend
+		if($this->getValue('staticResourcePath'))
+			$this->setValue('staticResourcePath', $this->addTrailingSlash($this->getValue('staticResourcePath')));
+		else
+			$this->setValue('staticResourcePath', '');
+
+		$this->buildManifest();
+		$this->buildSpine();
+
+	}
+
+	private function buildManifest()
+	{
+		$manifest = "";
+
+		$imageFiles = array();
+		foreach($this->content as $content)
+		{
+			/** @var ContentInterface $content */
+
+			if(!$this->getValue('manifestTemplate'))
+				$template = '<item id="{id}" media-type="{type}" href="{path}"/>' . "\n";
+			else
+				$template = $this->getValue('manifestTemplate');
+
+			$manifest .= $this->templatish->buildTemplate($template, array(
+				'type' => 'application/xhtml+xml',
+				'id'	=> self::MANIFEST_ITEM_PREFIX . $content->getUniqueIdentifier(),
+				'path'	=> $content->getAnchorPath()
+			));
+
+			$images = $this->htmlParser->extractImg($content->getHtml());
+			$imageFiles = array_unique(array_merge($imageFiles, $images));
+
+		}
+
+		array_walk($imageFiles, function(&$value)
+		{
+			$value = array(
+				'path' => $this->getValue('staticResourcePath') . $value,
+				'type' => $this->estimateMediaType($value),
+				'id'	=> rand(11111111,99999999)
+			);
+		});
+
+		foreach($imageFiles as $imageFile)
+		{
+			if(!$this->getValue('manifestTemplate'))
+				$template = '<item id="{id}" media-type="{type}" href="{path}"/>' . "\n";
+			else
+				$template = $this->getValue('manifestTemplate');
+
+			$manifest .= $this->templatish->buildTemplate($template, $imageFile);
+		}
+
+		$this->data['manifest'] = $manifest;
+
+	}
+
+	private function buildSpine()
+	{
+		$spin = "";
+		foreach($this->content as $content)
+		{
+			/** @var ContentInterface $content */
+
+		}
+	}
+
+	/**
+	 * Estimate the name of the image based on filetype
+	 * or file path
+	 *
+	 * todo: this is slipity-slipity, make it better or remove it
+	 *
+	 * @param $fileName
+	 */
+	private function estimateMediaType($fileName)
+	{
+		if(strpos($fileName, '.gif') !== false)
+			return 'image/gif';
+		else if(strpos($fileName, '.png') !== false)
+			return 'image/png';
+		else if(strpos($fileName, '.svg') !== false)
+			return 'image/svg+xml';
+		else if(strpos($fileName, '.jpg') !== false || strpos($fileName, '.jpeg') !== false)
+			return 'image/jpeg';
+
+	}
+
+	public function validate()
+	{
+		if(!$this->getValue('uniqueId'))
+			throw new \Exception("A uniqueId must be given to the OpfRenderer. No uniqueId present. Please supply in constructor or by setValue()");
+	}
 
     public function getXml()
     {
--- src/Develpr/Phindle/Renderers/XmlRenderer.php
+++ src/Develpr/Phindle/Renderers/XmlRenderer.php
@@ -1,6 +1,64 @@
 <?php namespace Develpr\Phindle;
 
 
-class XmlRenderer{
+abstract class XmlRenderer{
 
+	protected $templatish;
+	protected $templateFile;
+	protected $data;
+
+	public function __construct(Templatish $templatish = null)
+	{
+		$this->data = array();
+
+		if(is_null($templatish))
+			$this->templatish = new Templatish();
+		else
+			$this->templatish = $templatish;
+
+		$this->setupTemplate();
+	}
+
+	abstract public function render(array $attributes, array $content);
+
+	abstract protected function prepareForRender();
+
+	public function getValue($key)
+	{
+		if(array_key_exists($key, $this->data))
+			return $this->data[$key];
+		else
+			return false;
+	}
+
+	public function setValue($key, $value)
+	{
+		$this->data[$key] = $value;
+	}
+
+	protected function setupTemplate()
+	{
+		$this->templateFile = '';
+	}
+
+	/**
+	 * Removing a leading slash from a path
+	 * @param $path
+	 * @return mixed
+	 */
+	protected function removeLeadingSlash($path)
+	{
+		return (substr($path, 0, 1) == '/' ? substr_replace($path, "", 0, 1) : $path);
+	}
+
+	/**
+	 * Add a trailing slash to a path
+	 *
+	 * @param $path
+	 * @return string
+	 */
+	protected function addTrailingSlash($path)
+	{
+		return (substr($path, -1) == '/' ? $path : $path . '/');
+	}
 }
